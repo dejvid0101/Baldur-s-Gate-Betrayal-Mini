9873caea4b4758d06d6b8e6ed6351ae8d1b31b13
David Koren <dejvid0101@gmail.com>
2023-10-21 21:04:35 +0200

    first adventurer card image shown absolutely

--- src/main/java/me/projects/baldur/betrayal_at_baldurs_gate/HelloController.java
+++ src/main/java/me/projects/baldur/betrayal_at_baldurs_gate/HelloController.java
@@ -2,11 +2,20 @@ package me.projects.baldur.betrayal_at_baldurs_gate;
 
 import javafx.fxml.FXML;
 import javafx.scene.control.Label;
+import javafx.scene.image.Image;
+import javafx.scene.image.ImageView;
+import javafx.scene.layout.Pane;
 import me.projects.baldur.betrayal_at_baldurs_gate.classes.Adventurer;
 
 public class HelloController {
     @FXML
     public Label nameLabel;
+
+    @FXML
+    public Pane cardContainer;
+
+    @FXML
+    public ImageView cardImage;
 
     public Adventurer adventurer;
 
@@ -17,9 +26,14 @@ public class HelloController {
     }
 
     public void initialize() {
+
         if (nameLabel != null) {
             Adventurer adventurer = new Adventurer("Tasha");
             nameLabel.setText(adventurer.getName());
+        }
+
+        if (cardImage != null) {
+            cardImage.setImage(new Image("C:\\Users\\David\\OneDrive - Visoko uciliste Algebra\\Desktop\\Betrayal_at_Baldurs_Gate\\src\\main\\resources\\textures\\Lia_Faen_Tlabar.png"));
         }
     }
 }
